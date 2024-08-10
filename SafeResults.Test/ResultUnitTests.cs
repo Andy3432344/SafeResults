a708e6fede5c04f0c2405185acbf6fdda927d1e5
Andy3432344 <96932022+Andy3432344@users.noreply.github.com>
2024-08-10 11:33:08 -0500

    Fixed error OptionResult Tests.

--- SafeResults.Test/ResultUnitTests.cs
+++ SafeResults.Test/ResultUnitTests.cs
@@ -7,9 +7,9 @@ public class ResultUnitTests
 {
     private static Lazy<NoTestMachine> noTestMachine = new();
     private TestMachine machine = new();
-    protected const string BadString = "Don't use this value";
     protected const int BadInt = -1;
     protected readonly ITestMachine BadTestMachine = noTestMachine.Value;
+    protected const string BadString = "Don't use this value";
 
     [Fact]
     public void TestStringResults()
@@ -17,13 +17,14 @@ public class ResultUnitTests
         var opt = option(BadString);
         string actualString = Guid.NewGuid().ToString();
 
-        var testString = opt | machine.TestStringSuccess(actualString);
+        var testString = opt | machine.TestStringFail(actualString);
+        Assert.Equal(BadString, testString);
+        Assert.True(opt.None);
+
+        testString = opt | machine.TestStringSuccess(actualString);
         Assert.Equal(actualString, testString);
         Assert.True(opt.Some);
 
-        testString = opt | machine.TestStringFail(BadString);
-        Assert.Equal(BadString, testString);
-        Assert.True(opt.None);
     }
 
     [Fact]
@@ -52,7 +53,7 @@ public class ResultUnitTests
         Assert.IsAssignableFrom<TestMachine>(testMachine);
         Assert.True(opt.Some);
 
-        testMachine = opt | machine.TestClassFail(BadTestMachine);
+        testMachine = opt | machine.TestClassFail(actual);
         Assert.IsAssignableFrom<NoTestMachine>(testMachine);
         Assert.True(opt.None);
     }
